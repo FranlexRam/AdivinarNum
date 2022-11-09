e16e3fde8d5f15d31e13591538d423056ea67bd0
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-11-08 22:32:08 -0400

    Guess the number game

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,17 +1,34 @@
-alert('Elige dos numeros del 1 al 10');
-var num1 = 2;
-var num2 = 3;
-var n1c = num1;
-var n2c = num2;
+//Juego que tenga 3 intentos, que permita ingresar 2 numeros que sean del 1 al 10>
 
-var num1 = Number(prompt('Ingresa el numero 1:'));
-while (num1<1 || num1>10){
-    num1 = Number(prompt("Debe ingresar un numero valido"))
+
+var min = 1;
+var max = 10;
+var attempts = 0;
+var num1;
+var num2;
+var random = Math.ceil(Math.random()*10);
+alert('Please enter a number from ' +min+  ' to '+max);
+
+for (attempts; attempts<3; attempts++) {
+    num1 = parseInt(prompt('Enter the 1st number'));
+    num2 = parseInt(prompt('Enter the 2nd number'));
+
+    if (num1 >= min && num1 <=max && num2>=min && num2<=max){
+        if (num1 == random || num2 == random){
+            alert('Excellent, You guessed the number at the '+(attempts+1)+ ' attempt!!');
+        } else if (num1 !== random && num2 != random){
+            alert('Incorrect, You still have '+(2-attempts)+' attempts');
+        } else{
+            break;
+        }
+        
+    } else {
+        alert('Remember that num1 and num2 has to be from '+min+ ' to '+max);
+    }
 }
-
-// var num2 = Number(prompt('Ahora, ingresa el numero 2:'));
-
-
-alert('Ganaste');
-
+if (num1==random || num2==random){
+    alert('Congratulations! You guessed the number in the last attempt!');
+} else {
+    alert('Sorry, You have no more attempts left');
+}
 
